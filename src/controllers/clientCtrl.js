637d60398269d7ce49d9f6ee6c284b66ed8c4fcb
Syforce <adrian.puiu@barandisolutions.com>
2014-12-21 10:50:06 +0200

    implemented template for clients

--- src/controllers/clientCtrl.js
+++ src/controllers/clientCtrl.js
@@ -1,4 +1,34 @@
 angular.module('Kubicon')
-    .controller('ClientCtrl', function($rootScope) {
+    .controller('ClientCtrl', function($scope, $rootScope) {
         $rootScope.selectedPage = 5;
+
+        $scope.clients = [
+        	'./assets/images/client.png',
+        	'./assets/images/client.png',
+        	'./assets/images/client.png',
+        	'./assets/images/client.png',
+        	'./assets/images/client.png'
+        ];
+
+        $scope.getClientCss = function($index) {
+        	switch ($index % 5) {
+        		case 0: {
+        			return 'bl1 br3';
+        		}
+        		case 1: {
+        			return 'br1';
+        		}
+        		case 2: {
+        			return 'bt-1 br1';
+        		}
+        		case 3: {
+        			return 'bl4 br1';
+        		}
+        		case 4: {
+        			return 'bl1';
+        		}
+        	}
+
+        	return;
+        };
     });
